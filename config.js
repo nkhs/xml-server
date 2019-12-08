99a543410225d6563be16ff813317f8340e9056f
help <dd@d.com>
2019-12-08 13:31:17 +0800

    updated config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var port = 80;
 
-var host_url = `175.160.129.151`;
+var host_url = `35.184.34.167`;
 var client_url = `http://${host_url}/#`;
 var mongodb_url = `mongodb://localhost:27017/xml-db`;
 // var mongodb_url = `mongodb://192.168.1.105:27017/xml-db`;
