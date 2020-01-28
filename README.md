d7f27bae496c94aabc3a2b651eb79789fcececac
help <nkhs@yandex.com>
2020-01-28 20:41:57 +0800

    clean config

---
+++ README.md
@@ -0,0 +1,12 @@
+
+git clone https://github.com/nkhs/xml-client.git
+cd xml-client
+npm i
+ng build --prod
+Genrated 'dist' directory
+
+
+git clone https://github.com/nkhs/xml-server
+cd xml-server
+npm i
+pm2 start server.js
--- config.js
+++ config.js
@@ -1,13 +1,12 @@
 var port = 80;
 
 var host_url = `35.184.34.167`;
-// var host_url = `127.0.0.1`;
 var client_url = `http://${host_url}/#`;
 var mongodb_url = `mongodb://localhost:27017/xml-db`;
-// var mongodb_url = `mongodb://192.168.1.105:27017/xml-db`;
+
 
 var default_auth_email = "admin"
-var default_auth_password = "e10adc3949ba59abbe56e057f20f883e"
+var default_auth_password = "e10adc3949ba59abbe56e057f20f883e" // e10adc3949ba59abbe56e057f20f883e is md5('123456'), so password is 123456.
 
 module.exports = {
   DB: { URI: mongodb_url },
@@ -19,11 +18,9 @@ module.exports = {
   WEB_SOURCE: './public/dist',
   ADMIN: {
     auth_email: function () {
-      try { return require('./gmail.js').email; } catch (error) { }
       return default_auth_email;
     },
     auth_password: function () {
-      try { return require('./gmail.js').password; } catch (error) { }
       return default_auth_password;
     },
     uid: "ff00000000000000000000ff"
